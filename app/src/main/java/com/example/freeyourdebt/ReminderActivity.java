182fcd076b010c8e40cbda94f9541f9efc9fa670
jumipark1 <parkj71@student.douglascollege.ca>
2023-11-20 20:25:21 -0800

    make the remiderActivity java code and added the activity_reminder.xml code

--- app/src/main/java/com/example/freeyourdebt/ReminderActivity.java
+++ app/src/main/java/com/example/freeyourdebt/ReminderActivity.java
@@ -3,12 +3,41 @@ package com.example.freeyourdebt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ReminderActivity extends AppCompatActivity {
+    TextView debtorNameTextView;
+    TextView amountTextView;
+    TextView dueDateTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
+
+        debtorNameTextView = findViewById(R.id.debtorNameTextView);
+        amountTextView = findViewById(R.id.amountTextView);
+        dueDateTextView = findViewById(R.id.dueDateTextView);
+
+        // Retrieve data from the intent
+        long debtId = getIntent().getLongExtra("debtId", -1);
+        String debtorName = getIntent().getStringExtra("debtorName");
+        double amount = getIntent().getDoubleExtra("amount", 0.0);
+        long dueDateMillis = getIntent().getLongExtra("dueDate", 0);
+
+        // Format the date for display
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        String formattedDueDate = dateFormat.format(new Date(dueDateMillis));
+
+        // Set data in TextViews
+        debtorNameTextView.setText("Debtor: " + debtorName);
+        amountTextView.setText("Amount: $" + amount);
+        dueDateTextView.setText("Due Date: " + formattedDueDate);
     }
+
 }
